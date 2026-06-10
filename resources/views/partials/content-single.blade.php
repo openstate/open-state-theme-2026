c3cf4755cdc4cbb8854743bea87e5a5749b5fe63
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-06-10 13:45:42 +0200

    Finish responsiveness for posts

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,7 +1,7 @@
-<article class="bg-off-white-50 grid grid-cols-12 pt-[146px] pb-[160px]" @php(post_class('h-entry'))>
-  <div class="col-span-6 col-start-4">
+<article class="md:grid md:grid-cols-12 xl:grid-cols-12 px-[16px] pt-[146px] pb-[100px] xl:pb-[160px]" @php(post_class('h-entry'))>
+  <div class="md:col-span-8 md:col-start-3 xl:col-span-6 xl:col-start-4">
     <header>
-      <h1 class="p-name font-serif text-[2.75rem]/[3rem] tracking-[-0.0375rem] pb-[20px]">
+      <h1 class="p-name font-serif text-[1.5rem]/[1.75rem] tracking-[-0.00625rem] md:text-[2.25rem]/[2.5rem] md:tracking-[-0.025rem] xl:text-[2.75rem]/[3rem] xl:tracking-[-0.0375rem] pb-[20px]">
         {!! $title !!}
       </h1>
 
