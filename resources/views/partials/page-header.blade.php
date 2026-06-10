c9493cc4803d26fac7b9abc69162f1a28f195f8f
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-06-10 13:28:45 +0200

    Add heading styles

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
 <div class="page-header mb-[16px] md:mb-[24px] xl:mb-[40px]">
-  <h1 class="font-serif text-[1.75rem]/[2rem] tracking-[-0.0125rem] md:text-[2.75rem]/[3rem] md:tracking-[-0.0375rem] xl:text-[3.25rem]/[3.5rem] xl:tracking-[-0.8px]">{!! $title !!}</h1>
+  <h1 class="text-[1.75rem]/[2rem] tracking-[-0.0125rem] md:text-[2.75rem]/[3rem] md:tracking-[-0.0375rem] xl:text-[3.25rem]/[3.5rem] xl:tracking-[-0.8px]">{!! $title !!}</h1>
 </div>
