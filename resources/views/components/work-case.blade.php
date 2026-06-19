8753d03297ea352f8d9ed96be4c7cee0e5fd833e
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-06-19 11:46:57 +0200

    Small fix

--- resources/views/components/work-case.blade.php
+++ resources/views/components/work-case.blade.php
@@ -8,11 +8,9 @@
 ])
 
 <div class="snap-start flex flex-col md:flex-row min-h-[528px] md:h-[420px] xl:h-[520px] min-w-[288px] md:min-w-[688px] xl:min-w-[1100px] {{ $bgColor }} {{ $textColor }} rounded-lg overflow-hidden">
-  <div class="flex flex-col order-2 md:order-1 p-[24px] pt-[20px] md:p-[32px]">
-    <div class="flex-1">
-      <h3 class="mt-0 mb-[8px] md:mb-[12px] xl:mb-[16px] text-[1.5rem]/[1.75rem] tracking-[-0.00625rem] md:text-[2.25rem]/[2.5rem] md:tracking-[-0.025rem] xl:text-[2.75rem]/[3rem] xl:tracking-[-0.0375rem]">{{ $title }}</h3>
-      <p class="tracking-[-0.00625rem] xl:text-[1.125rem]/[1.75rem]">{{ $slot }}</p>
-    </div>
+  <div class="flex flex-col flex-1 order-2 md:order-1 p-[24px] pt-[20px] md:p-[32px]">
+    <h3 class="mt-0 mb-[8px] md:mb-[12px] xl:mb-[16px] text-[1.5rem]/[1.75rem] tracking-[-0.00625rem] md:text-[2.25rem]/[2.5rem] md:tracking-[-0.025rem] xl:text-[2.75rem]/[3rem] xl:tracking-[-0.0375rem]">{{ $title }}</h3>
+    <p class="tracking-[-0.00625rem] xl:text-[1.125rem]/[1.75rem]">{{ $slot }}</p>
     <div class="mt-auto flex max-md:flex-col gap-[10px]">
       @if ($logo1)
       <div class="bg-white rounded-lg w-[184px] h-[76px] pt-[12px] px-[10px] pb-[14px]">
