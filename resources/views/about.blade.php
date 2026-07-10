eceafe0debc90f4a4a5c570b39484f83d3c0a809
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-07-10 17:01:00 +0200

    Add gradient decoration to about page hero

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,15 +8,36 @@
   @while(have_posts()) @php(the_post())
     <div class="bg-purple-800 text-white px-[16px] md:px-[24px] pt-[48px] md:pt-[120px] pb-[80px] md:pb-[200px]">
       <div class="mx-auto w-full max-w-[1920px] md:grid md:grid-cols-12 xl:gap-x-[64px]">
-        <div class="md:col-span-8 col-start-1 xl:col-span-6 mb-[16px] xl:mb-[40px]">
+        <div class="md:col-span-8 xl:col-span-6 mb-[16px] xl:mb-[40px] flex flex-col">
           <h1 class="font-serif text-[2.25rem]/[2.5rem] tracking-[-0.025rem] md:text-[3.25rem]/[3.5rem] md:tracking-[-0.05rem] xl:text-[4rem]/[4.25rem] xl:tracking-[-0.0625rem] m-0">Zonder transparantie geen democratie</h1>
+
+          <span class="md:hidden absolute top-[100px] ml-[17.40%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[142px] h-[5px]"></span>
+
+          <span class="hidden md:block xl:hidden absolute top-[120px] ml-[17.40%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[142px] h-[5px]"></span>
+          <span class="hidden md:block xl:hidden absolute top-[178px] ml-[88%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[30px] h-[5px]"></span>
+          <span class="hidden md:block xl:hidden absolute top-[445px] ml-[83%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[78px] h-[5px]"></span>
+
+          <span class="hidden xl:block mt-[147px] ml-[21.75%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[170px] h-[5px]"></span>
+          <span class="hidden xl:block mt-[63px] ml-[64.20%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[30px] h-[5px]"></span>
+          <span class="hidden xl:block mt-[98px] ml-[30.82%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[186px] h-[5px]"></span>
+          <span class="hidden xl:block absolute top-[750px] ml-[1.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[85px] h-[5px]"></span>
+          <span class="hidden xl:block absolute top-[820px] ml-[25%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[78px] h-[5px]"></span>
         </div>
 
-        <div class="md:col-span-8 col-start-1 xl:col-span-6 xl:h-[520px] flex flex-col">
+        <div class="md:col-span-8 xl:col-span-6 xl:h-[520px] flex flex-col">
+          <span class="hidden xl:block mt-[67px] ml-[22.36%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[85px] h-[5px]"></span>
           <p class="xl:w-[65%] mt-[12px] md:mt-[16px] xl:mt-auto mb-[40px] md:mb-[56px] tracking-[-0.00625rem] xl:text-[1.125rem]/[1.75rem] xl:tracking-[-0.00625rem]">
             Open State Foundation is een onafhankelijke stichting die zich inzet voor een digitaal transparante en controleerbare overheid. Sinds 2012 bouwen we tools, doen we onderzoek en adviseren we overheden — altijd vanuit de overtuiging dat een sterke democratie begint bij openheid.
           </p>
           <x-button type="button" text="Lees hele verhaal" icon="plus" variant="secondary" onclick="document.getElementById('verhaal').showModal()" arialabel="dialog" />
+          <div class="relative">
+            <span class="md:hidden absolute top-[-56px] ml-[60%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[100px] h-[5px]"></span>
+            <span class="md:hidden absolute top-[40px] ml-[12%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[143px] h-[5px]"></span>
+
+            <span class="hidden md:block xl:hidden absolute top-[75px] ml-[12%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[143px] h-[5px]"></span>
+            <span class="hidden md:block xl:hidden absolute top-[132px] ml-[70%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[215px] h-[5px]"></span>
+          </div>
+          <span class="hidden xl:block absolute top-[810px] ml-[18%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[215px] h-[5px]"></span>
           <x-popup title="Een overheid <span class='text-pink'>zonder geheimen</a>" id="verhaal">
             <p>In een sterke democratie heeft de overheid geen geheimen. Wij geloven in een open overheid waarbij burgers, journalisten en ambtenaren toegang hebben tot informatie om op te kunnen vertrouwen en op te kunnen bouwen.</p>
 
