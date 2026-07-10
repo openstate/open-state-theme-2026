cd1c5839f8723e6eebe06751d486352b0703455b
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-07-10 23:27:11 +0200

    Add gradient decoration to 404 page for smaller breakpoints

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -6,6 +6,18 @@
     <h1 class="mb-[40px] md:mb-[56px] text-white text-[2.25rem]/[2.5rem] tracking-[-0.025rem] md:text-[3.25rem]/[3.5rem] md:tracking-[-0.05rem] xl:text-[4rem]/[4.25rem] xl:tracking-[-0.0625rem]"><span class="text-pink">404</span><br class="md:hidden"> Pagina niet gevonden</h1>
     <x-button text="Naar homepage" variant="secondary" href="/" />
 
+    <span class="md:hidden absolute top-[16%] ml-[56.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[70px] h-[5px]"></span>
+    <span class="md:hidden absolute top-[25%] ml-[58.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[125px] h-[5px]"></span>
+    <span class="md:hidden absolute top-[75%] ml-[64.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[90px] h-[5px]"></span>
+    <span class="md:hidden absolute top-[80%] ml-[60.2%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[35px] h-[5px]"></span>
+    <span class="md:hidden absolute top-[85%] ml-[54.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[125px] h-[5px]"></span>
+
+    <span class="hidden md:block xl:hidden absolute top-[36%] ml-[86.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[70px] h-[5px]"></span>
+    <span class="hidden md:block xl:hidden absolute top-[45%] ml-[-86.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[130px] h-[5px]"></span>
+    <span class="hidden md:block xl:hidden absolute top-[75%] ml-[-70.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[90px] h-[5px]"></span>
+    <span class="hidden md:block xl:hidden absolute top-[80%] ml-[70.2%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[35px] h-[5px]"></span>
+    <span class="hidden md:block xl:hidden absolute top-[85%] ml-[84.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[155px] h-[5px]"></span>
+
     <span class="hidden xl:block absolute top-[10%] ml-[-74.2%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[160px] h-[5px]"></span>
     <span class="hidden xl:block absolute top-[16%] ml-[72.2%] bg-linear-[270deg,#3D1535_0%,#FF29B4_100%] w-[101px] h-[5px]"></span>
     <span class="hidden xl:block absolute top-[36%] ml-[86.2%] bg-linear-[270deg,#FF29B4_0%,#3D1535_100%] w-[70px] h-[5px]"></span>
