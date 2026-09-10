0ad24e828619f632d0285a9a3713c240cb285769
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-09-10 13:13:48 +0200

    Small fix: removes [:nl] qtranslate tag at another place

--- resources/views/partials/hero-badge-white.blade.php
+++ resources/views/partials/hero-badge-white.blade.php
@@ -3,7 +3,7 @@
   $post_or_page = get_post_type();
   if ($post_or_page == 'post') {
     $categories = wp_get_post_categories(get_the_ID(), ['fields' => 'all']);
-    $badge = $categories ? $categories[0]->name : '';
+    $badge = $categories ? apply_filters('single_cat_title', $categories[0]->name) : '';
   } else {
     $parent_id = wp_get_post_parent_id(get_the_id());
     if ($parent_id) {
