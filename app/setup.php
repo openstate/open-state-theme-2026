3a638c9a1325e8ab77afd5051ec0ccaaa3c51a42
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-09-16 10:57:22 +0200

    Change excerpt cutoff to ...

--- app/setup.php
+++ app/setup.php
@@ -212,3 +212,7 @@ add_action('init', function () {
 add_filter('excerpt_length', function ($length) {
     return 20;
 }, 999);
+
+add_filter('excerpt_more', function () {
+    return '&hellip;';
+});
